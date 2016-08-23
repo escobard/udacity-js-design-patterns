d682341feafa692117a08e87f3fe9e3b80ddc2ba
Daniel Escobar <escobard90@gmail.com>
2016-08-22 23:10:41 -0600

    Added code for testing second cat, and copied source code from : http://stackoverflow.com/questions/13495010/how-to-add-content-to-html-body-using-javascript

--- cat-clicker/js/cat-clicker.js
+++ cat-clicker/js/cat-clicker.js
@@ -1,5 +1,17 @@
+// First Cat Click function
+
  var clicks = 0;
     function catClicked() {
         clicks += 1;
         document.getElementById("clicks").innerHTML = clicks;
     };
+	
+// Second Cat Click function
+
+ var clicks = 0;
+    function catClicked2() {
+        clicks += 1;
+        document.getElementById("clicks").innerHTML = clicks;
+    };
+	
+document.getElementById("parentID").appendChild();
