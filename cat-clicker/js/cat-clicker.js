9b6b72c9c60aa7cd0b837ea884115a4d3ae2a2c5
Daniel Escobar <escobard90@gmail.com>
2016-08-24 21:45:09 -0600

    Prototype branch first commit

--- cat-clicker/js/cat-clicker.js
+++ cat-clicker/js/cat-clicker.js
@@ -41,3 +41,30 @@ function catTitle2() {
     list2.insertBefore(catTitleContainer2, list2.childNodes[0]);
 }
 window.addEventListener("load", catTitle2, false);
+
+// JS code that needs to be changed to fit the requirements of the assignment here : https://classroom.udacity.com/courses/ud989/lessons/3417188540/concepts/34338690500923
+
+// clear the screen for testing
+document.body.innerHTML = '';
+
+var nums = [1,2,3];
+
+// Let's loop over the numbers in our array
+for (var i = 0; i < nums.length; i++) {
+
+    // This is the number we're on...
+    var num = nums[i];
+
+    // We're creating a DOM element for the number
+    var elem = document.createElement('div');
+    elem.textContent = num;
+
+    // ... and when we click, alert the value of `num`
+    elem.addEventListener('click', (function(numCopy) {
+        return function() {
+            alert(numCopy);
+        };
+    })(num));
+
+    document.body.appendChild(elem);
+};
