e8348191e9ed497039e74e4f9d768d5465564b50
Daniel Escobar <escobard90@gmail.com>
2016-09-08 17:46:37 -0600

    Added responsive styling

--- cat-clicker/js/cat-clicker.js
+++ cat-clicker/js/cat-clicker.js
@@ -1,17 +1,15 @@
 // Cat clicker premium v2
 
-// Cat Container initiation	
+// Cat Container
 function catContainer() {
 	
-// variables
-
+// Variables
 var catTitleElement = document.createElement("H1");
 var catContainerImg = document.createElement("img")
 var list = document.getElementById("cat-container");
 var catSecondaryTitleElement = document.createElement("H3");
 
 // Attributes
-
 catContainerImg.setAttribute("id", "catImage");
 catTitleElement.setAttribute("id", "catTitle");
 catTitleElement.setAttribute("class", "cat-title");
@@ -24,7 +22,7 @@ list.insertBefore(catTitleElement, list.childNodes[0]);
 }
 window.addEventListener("load", catContainer, false);
 
-// Variable for the cat Menu structure.
+// Cat Menu
 function catMenu() {
 var catTitleContainer = document.createElement("div");
 var catTitleElement = document.createElement("H2");
@@ -33,7 +31,7 @@ var catContainerIntroText = document.createTextNode("Click the image below to in
 var catMenuContainer = document.createElement("ul")
 var list = document.getElementById("cat-container")
 
-// creates the array for the cat Menu Items
+// Creates the array for the cat Menu Items
 var catMenuItemLength = document.createDocumentFragment();
 
 var catMenuItems = [
@@ -44,24 +42,21 @@ var catMenuItems = [
   catMenuItem("Roger")
 ]
 
-// creates the loop for each value within the cat menu items array.
+// Creates the loop for each value within the cat menu items array.
 for(var i = 0; i < catMenuItems.length; i++) {
   catMenuItemLength.appendChild(catMenuItems[i]);
 }
 
-// appends the name of each cat to the button
+// Appends the name of each cat to the button
 function catMenuItem(text) {
   var catMenuItemContainer = document.createElement("li");
   var catMenuItemButton = document.createElement("button");
+  var catMenuTitle = catMenuItemButton.appendChild(document.createTextNode(text));
   catMenuItemButton.setAttribute("id", text + "-button")
   catMenuItemButton.setAttribute("class", "btn" + " btn-default")
-// append button children  
   catMenuItemContainer.appendChild(catMenuItemButton);
-  var catMenuTitle = catMenuItemButton.appendChild(document.createTextNode(text));
   return catMenuItemContainer;
 }
-
-// Appends all catMenuItems at once
 
 // Attributes
 catTitleContainer.setAttribute("class", "cat-title-container");
@@ -79,8 +74,6 @@ list.insertBefore(catTitleContainer, list.childNodes[0]);
 }
 window.addEventListener("load", catMenu, false);
 
-// Cat Objects and cat menu functions - cat clicker premium v2 end
-
 // used this to learn how to build objects https://discussions.udacity.com/t/honest-opinion-on-cat-clicker-5-cats/24532
 // thanks to this forum post, I have learned the importance of using objects for elements with similar attributes to build scalable apps.
 // full credit for the original code goes to User : PGS82
@@ -91,23 +84,26 @@ function catLoad(){
 	// gets the 'catCount' div element, converts it into a variable. 
 	var catClick = document.getElementById("catCount");
     
-	// creates the variable for cat button
+	// creates the variable for cat button & creates a function for the selector variable
 	var CatButtonObject = function(selector){
-	// creates a function for the selector variable
+		
 	// defines the element ID of selector. 
 	this.element = document.getElementById(selector);
+	
 	// defines the click count,
 		this.catClickCount = 0;
 	};
     
 	// adds an event function for when a CatButtonObject object is clicked. Don't quite understand this, but will need to research more on prototype and events.
 	CatButtonObject.prototype.addEvent = function(event, fn){
+		
 	//adds the event listener to the element which id's has been identified.
 		this.element.addEventListener(event, fn);
 	};	
 
 	// adds the object for a cat
 	var Cat = {
+		
 		// adds the image value
 		image: document.getElementById("catImage"),
 		
@@ -136,6 +132,7 @@ function catLoad(){
 
 	//adds event for each cat button 	
 	catButtonOne.addEvent("click",
+	
 	// sets the function to set object values
 	function(){
 		addCatInfo(this, "img/cat-picture.jpg", "Ollie");
@@ -161,16 +158,19 @@ function catLoad(){
 
 	// creates the variable that adds values to each CatButtonObject object 
 	function addCatInfo(selfElement, catSrc, catTitle){
+		
 		// sets the value of the the setSrc variable, converting it into the catSrc variable 
 		Cat.setSrc(catSrc);
+		
 		// sets the value of setTitleText variable, converting it into the catTitle variable
 		Cat.setTitleText(catTitle);
+		
 		// increments the value of the catClickCount variable
 		selfElement.catClickCount++;
+		
 		// sets the inner html value of the catClickCount variable to the number of clicks currently saved by the object.
 		catClick.innerHTML = selfElement.catClickCount;
 	}
-
 }
 window.addEventListener("load", catLoad, false);
 
