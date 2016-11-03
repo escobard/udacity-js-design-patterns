42b0deeb661df34eaa54c4264d832d2cb164311f
Daniel Escobar <escobard90@gmail.com>
2016-11-03 17:12:05 -0600

    type: documentation
    further commented the MVO application

--- ud989-pizzamvo/js/app.js
+++ ud989-pizzamvo/js/app.js
@@ -45,7 +45,9 @@ $(function() {
             view.render();
         },
 
+// creates the octopus.getVisiblePizzas method function that takes pizza as an argument where
         getVisiblePizzas: function() {
+
             var visiblePizzas = data.pizzas.filter(function(pizza) {
                 return pizza.visible;
             });
@@ -57,8 +59,10 @@ $(function() {
         }
     };
 
-
+// the view grabs elements from the DOM that we will use on other parts of the application. 
+// attempt to do this for all applications I build moving forward.
     var view = {
+// adds an event listener for the button which activates the add pizza function
         init: function() {
             var addPizzaBtn = $('.add-pizza');
             addPizzaBtn.click(function() {
@@ -79,15 +83,21 @@ $(function() {
             this.render();
         },
 
+// tells the view to render itself
+// in a nutshell clears the pizza list entirely, and renders all visible pizzas each time. 
         render: function() {
             // Cache vars for use in forEach() callback (performance)
             var $pizzaList = this.$pizzaList,
                 pizzaTemplate = this.pizzaTemplate;
 
             // Clear and render
+            // sets the html of the .pizza-list to an empty string
             $pizzaList.html('');
+            // retrieves visible pizzas from the controler (octopus)
             octopus.getVisiblePizzas().forEach(function(pizza) {
-                // Replace template markers with data
+                // for each exsisting pizza object, replace 
+                // Replace template markers with data in the pizza object.
+                // Fills the template HTML with the pizza ID, and adding it to the pizza list
                 var thisTemplate = pizzaTemplate.replace(/{{id}}/g, pizza.id);
                 $pizzaList.append(thisTemplate);
             });
