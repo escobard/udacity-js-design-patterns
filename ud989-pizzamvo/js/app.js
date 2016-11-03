663809b6d43b370ab6b37da54209f6deb2fb1f28
Daniel Escobar <escobard90@gmail.com>
2016-11-03 00:03:52 -0600

    type: documentation
    added commentary within the app.js file to better understand authors programming

--- ud989-pizzamvo/js/app.js
+++ ud989-pizzamvo/js/app.js
@@ -1,25 +1,47 @@
 $(function() {
-
+// creates the data object variable 
     var data = {
+
+// sets data.lastID = 0
         lastID: 0,
+// creates an empty array for data.pizzas
         pizzas: []
     };
 
-
+// creates the octopus object variable
     var octopus = {
+
+// creates the octopus.addPizza method function where:
         addPizza: function() {
+
+// the variable thisID = is the value of the incremental addition of the data.lastID value
             var thisID = ++data.lastID;
 
+// pushes the value of the thisID variable into the data.pizzas array as an item. 
             data.pizzas.push({
+
+// sets the method of the the pizzas array item value where:
+// data.pizzas.[arraynumber].id = thisID.
                 id: thisID,
+
+// data.pizzas.[arraynumber].visible = true.
                 visible: true
             });
+
+// returns the view.render function
             view.render();
         },
 
+// create the octopus.removePizza method function that takes pizza as an argument where: 
         removePizza: function(pizza) {
+
+// the variable clickedPizza = data.pizzas[pizza.id -1], so sets the id of the pizza array item method to 1 less
             var clickedPizza = data.pizzas[ pizza.id - 1 ];
+
+// sets the current clickedPizza.visible arry item method to false
             clickedPizza.visible = false;
+
+// returns the view.render function
             view.render();
         },
 
